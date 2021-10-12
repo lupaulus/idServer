494bc784275a0526516b0fa0dcff9b36d95877ec
Lucas PAULUS <lucas67500@gmail.com>
2021-10-12 14:29:52 +0200

    ID Serveur fini chef

--- Student.IdentityServer/Quickstart/Home/HomeController.cs
+++ Student.IdentityServer/Quickstart/Home/HomeController.cs
@@ -29,14 +29,12 @@ namespace IdentityServerHost.Quickstart.UI
 
         public IActionResult Index()
         {
-            if (_environment.IsDevelopment())
-            {
-                // only show in development
-                return View();
-            }
 
-            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
-            return NotFound();
+            // only show in development
+            return View();
+
+            // _logger.LogInformation("Homepage is disabled in production. Returning 404.");
+            // return NotFound();
         }
 
         /// <summary>
