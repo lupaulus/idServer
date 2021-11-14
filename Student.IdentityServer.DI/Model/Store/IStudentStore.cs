475d2e27b27dc6580486f03e724e5a2063e7104d
Lucas Paulus <lucas67500@gmail.com>
2021-11-14 22:55:30 +0100

    update all

--- Student.IdentityServer.DI/Model/Store/IStudentStore.cs
+++ Student.IdentityServer.DI/Model/Store/IStudentStore.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 
-namespace Student.IdentityServer.Model.Store
+namespace Student.IdentityServer.DI.Model.Store
 {
     public interface IStudentStore : IUserStore<StudentUser>
     {
--- Student.IdentityServer/Quickstart/Account/AccountController.cs
+++ Student.IdentityServer/Quickstart/Account/AccountController.cs
@@ -17,7 +17,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Student.IdentityServer.Model.Store;
+using Student.IdentityServer.DI.Model.Store;
 
 namespace IdentityServerHost.Quickstart.UI
 {
